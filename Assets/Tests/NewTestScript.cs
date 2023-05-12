9d614744e26a0481539c3642484e55bd8706e081
pruss <31600998+zxc30314@users.noreply.github.com>
2023-05-12 21:19:11 +0800

    Update NewTestScript.cs

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -16,7 +16,6 @@ public class NewTestScript
     [UnityTest]
     public IEnumerator NewTestScriptWithEnumeratorPasses()
     {
-        
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
         yield return null;
